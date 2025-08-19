d64415c9701f9e889777c4bf1e82471c8e02f540
mooner92 <chlaudgjs0989@gmail.com>
2025-08-20 00:14:58 +0900

    set CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // 모든 오리진(origin)을 허용합니다. Figma의 'null' 오리진도 포함됩니다.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(8000);
 }
 bootstrap();
