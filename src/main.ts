ce605d9af9f1bf7f888b0f151ba0631ad8c464df
mooner92 <chlaudgjs0989@gmail.com>
2025-08-21 16:24:33 +0900

    add privary policy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  // 정적 파일 서빙 설정
+  app.useStaticAssets(join(__dirname, '..', 'src', 'policy'), {
+    prefix: '/policy',
+  });
+  
   app.enableCors({
     origin: '*', // 모든 오리진(origin)을 허용합니다. Figma의 'null' 오리진도 포함됩니다.
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
